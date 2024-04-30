42df352e939ec3db683b454e1a1fa6b0da92fdf4
paolacmoura <paola.moura.92@gmail.com>
2024-04-30 20:55:57 -0300

    exercicio 10 resolvido

--- lista1exercicios.js
+++ lista1exercicios.js
@@ -209,3 +209,14 @@ if (codigoOrigem === 1){
 } else {
     console.log("Produto importado");
 }
+
+
+/*
+    Escreva um algoritmo para ler um número inteiro e escrevê-lo na tela 10 vezes.
+*/
+
+let numero = parseInt(prompt("Digite um número: "));
+
+for (let i = 0; i < 10; i ++){
+    console.log(numero);
+}
