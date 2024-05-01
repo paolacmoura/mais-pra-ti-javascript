959c2121ada44938d2263be72f12f6b17b504897
paolacmoura <paola.moura.92@gmail.com>
2024-04-30 21:24:15 -0300

    exercicio 12 resolvido

--- lista1exercicios.js
+++ lista1exercicios.js
@@ -217,7 +217,7 @@ if (codigoOrigem === 1){
 
 let numero = parseInt(prompt("Digite um número: "));
 
-for (let i = 0; i < 10; i ++){
+for (let i = 0; i < 10; i++){
     console.log(numero);
 }
 
@@ -238,4 +238,16 @@ while(insiraNumero === null || insiraNumero > 0){
         console.log(`O número ${insiraNumero} é ímpar.`);
     }
     insiraNumero = parseInt(prompt("Insira um número: "));
+}
+
+
+/*
+    Escreva um algoritmo que gere os números de 1000 a 1999 e escreva aqueles que,
+    divididos por 11, dão resto igual a 5.
+*/
+
+for(let i = 1000; i < 2000; i++){
+    if(i % 11 === 5){
+        console.log(i);
+    }
 }
