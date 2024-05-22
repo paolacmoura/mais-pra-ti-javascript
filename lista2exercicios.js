027d5b58749d20ee9378d1e5d5bbf06dc4cda315
paolacmoura <paola.moura.92@gmail.com>
2024-05-21 22:24:17 -0300

    lista 2 exercicio 16

--- lista2exercicios.js
+++ lista2exercicios.js
@@ -434,3 +434,26 @@ for(let i = 0; i < 10; i++){
 for(let obj of meusPares){
     console.log(`Na posição ${obj.posicao}, o valor par digitado foi ${obj.valor}`);
 }
+
+
+/*
+    Crie uma lógica que preencha um vetor de 20 posições com números aleatórios
+    (entre 0 e 99) gerados pelo computador. Logo em seguida, mostre os números gerados e
+    depois coloque o vetor em ordem crescente, mostrando no final os valores ordenados.
+*/
+
+
+let vetor = [];
+
+for (let i = 0; i < 20; i++) {
+    let numeroAleatorio = Math.floor(Math.random() * 100);
+    vetor.push(numeroAleatorio);
+}
+
+console.log("Números gerados:");
+console.log(vetor);
+
+vetor.sort((a, b) => a - b);
+
+console.log("Números ordenados:");
+console.log(vetor);
